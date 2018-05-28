60ec700891dfe2a2e7778f57a800d38861994621
maqnico <nicolasmq@t-online.de>
2018-05-28 19:02:47 +0200

    :rabbit: :wrench: added login
    deleted obsolete html files, added some id´s, changed some names and implemented the login

--- RezeptDB.js
+++ RezeptDB.js
@@ -2,3 +2,40 @@
     newRecipe edit recipe
     recipe fetch(favourites, selfmade, bookmarked, by attributes/ ingredients)
 */
+
+//login
+const inpFldUsername = document.querySelector("#Nname");
+const inpFldPW = document.querySelector("#PWort");
+const lblUsername = document.querySelector("#NnameLabel");
+const lblPW = document.querySelector("#PWortLabel");
+
+//jump to password field
+inpFldUsername.addEventListener("keydown", event => {
+  if (event.key === "Enter") {
+    inpFldPW.focus();
+  }
+});
+
+inpFldPW.addEventListener("keydown", event => {
+  if (event.key !== "Enter") {
+    return;
+  }
+  // Check username for whitespace
+  if (/\s+/.test(inpFldUsername.value) === true) {
+    lblUsername.value = "Bitte Nutzername eingeben";
+    lblUsername.style.color = "red";
+    return;
+  }
+  // Check password for whitespace
+  if (/\s+/.test(inpFldPW.value) === true) {
+    lblPW.value = "Bitte Passwort eingeben";
+    lblPW.style.color = "red";
+    return;
+  }
+  login(inpFldUsername.value, inpFldPW.value);
+});
+
+function login(UN, PW) {
+  window.location.assign("Hauptseite.html");
+  let sessionUserName = inpFldUsername.value;
+}
