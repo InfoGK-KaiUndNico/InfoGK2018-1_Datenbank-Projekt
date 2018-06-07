c222716f8ea190c3768e3bce985e91116d2abab3
maqnico <nicolasmq@t-online.de>
2018-06-07 12:24:22 +0200

    //Comment-loadZutatenByIDs

--- frontend/src/lib/util/loadZutatenByIds.ts
+++ frontend/src/lib/util/loadZutatenByIds.ts
@@ -2,6 +2,7 @@ import getCommonHeaders from './getCommonHeaders';
 
 export default async function loadRecipesByIds(zutaten: string[]) {
 	const fullZutaten: any[] = [];
+	// get zutaten data for all zutaten found in search
 	for (const zutatName of zutaten) {
 		const response = await fetch(`http://localhost:4000/ingredients/${zutatName}`, {
 			headers: getCommonHeaders(),
